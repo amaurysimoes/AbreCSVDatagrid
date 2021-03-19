920809dc2028ae6e3fe4b77e21bc56b0a55d0e37
Amaury Huerb Simões <31355114+amaurysimoes@users.noreply.github.com>
2021-03-19 17:43:15 -0400

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AbrirCSV
+{
+    public partial class AbreCSV : Form
+    {
+        public AbreCSV()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DataTable dt = new DataTable();
+
+            string[] Linha = System.IO.File.ReadAllLines(@"C:\Teste\teste.csv");
+
+            for (Int32 i = 0; i < Linha.Length; i++)
+            {
+                string[] campos = Linha[i].Split(Convert.ToChar(";"));
+
+                if (i == 0)
+                {
+                    for (Int32 i2 = 0; i2 < campos.Length; i2++)
+                    {
+                        DataColumn col = new DataColumn();
+                        dt.Columns.Add(col);
+                    }
+                }
+                dt.Rows.Add(campos);
+            }
+            dataGridView1.DataSource = dt;
+        }
+    }
+}
